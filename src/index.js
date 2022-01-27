6d07378488d2c15790ea05ae8d5e613ea5d12fa8
Nguyễn Hoàng Anh <nguyenhoanganh@luvina.net>
2022-01-27 17:30:29 +0700

    demo js app structure

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Product = require('./model/product');
 
+const categories = ['fruit', 'vagetable', 'dairy'];
+
 mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log('CONNECTION OPEN :v');
@@ -21,11 +23,17 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
 app.get('/products', async (req, resp) => {
-    const products = await Product.find({});
-    resp.render('products/index', { products });
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        resp.render('products/index', { products, category, categories });
+    } else {
+        const products = await Product.find({});
+        resp.render('products/index', { products, category: 'All', categories });
+    }
 });
 app.get('/products/new', (req, resp) => {
-    resp.render('products/new');
+    resp.render('products/new', { categories });
 })
 app.post('/products', async (req, resp) => {
     const newProduct = new Product(req.body);
@@ -40,14 +48,18 @@ app.get('/products/:id', async (req, resp) => {
 app.get('/products/:id/edit', async (req, resp) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    resp.render('products/edit', { product });
+    resp.render('products/edit', { product, categories });
 })
 app.put('/products/:id', async (req, resp) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
     resp.redirect(`/products/${product.id}`)
 })
-
+app.delete('/products/:id', async (req, resp) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    resp.redirect('/products');
+})
 
 app.listen(3000, () => {
     console.log('App is listening on port 3000');
